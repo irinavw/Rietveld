e611470600acc225282bbad23422c11cedc5b21e
Irina <ivw.etc@gmail.com>
2019-04-19 17:25:59 -0400

    Final Update April 19

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
+  $(document).ready(function () {
+    var back = ["red","blue", "yellow"];
+    var rand = back[Math.floor(Math.random() * back.length)];
+    $("body").css("border-color", rand);
+    $("h3").css("color", rand);
+    });
   $(".chairback").hover(function() {
     $("h2").slideToggle();
     });
   $(".chairback, .chairslant").hover(function() {
     $(this).toggleClass("blackStroke");
     $("p").toggleClass("blackText");
+    var back = ["red","blue", "yellow"];
+    var rand = back[Math.floor(Math.random() * back.length)];
+    $(".blackStroke").css("outline-color", rand);
     });
 });
