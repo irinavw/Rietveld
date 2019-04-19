73c906849c921fdd660e9afa9f4b66f9847d2504
Irina <ivw.etc@gmail.com>
2019-04-19 12:22:00 -0400

    Update April 19

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
-
-
+  $(".chairback").hover(function() {
+    $("h2").slideToggle();
+    });
+  $(".chairback, .chairslant").hover(function() {
+    $(this).toggleClass("blackStroke");
+    $("p").toggleClass("blackText");
+    });
 });
